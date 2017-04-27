a572d21eb1f839df0d1c0c393e815f0b8b0705db
Felipe Vega <fvega3@calstatela.edu>
2017-04-27 00:47:51 -0700

    Add Blob Download From Image Paths
    - This commit adds the ability to download images from the paths from the SQL Database
    - This commit also adds the ability to download the images in order of most recently uploaded

--- PhotoArchive/HistoryViewController.swift
+++ PhotoArchive/HistoryViewController.swift
@@ -21,7 +21,9 @@ class HistoryViewController: UIViewController, UICollectionViewDelegate, UIColle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let account = try! AZSCloudStorageAccount(fromConnectionString: "SharedAccessSignature=sv=2016-05-31&ss=b&srt=o&sp=rw&se=2027-02-24T00:00:00Z&st=2017-02-24T00:00:00Z&spr=https&sig=kChTx0B8faa43g%2F%2F2G5LIWBCOKMxq1eIgqOUn9Ds9s4%3D;BlobEndpoint=https://boephotostore.blob.core.windows.net")
+        let sas = "sv=2016-05-31&ss=b&srt=o&sp=rw&se=2027-02-24T00:00:00Z&st=2017-02-24T00:00:00Z&spr=https&sig=kChTx0B8faa43g%2F%2F2G5LIWBCOKMxq1eIgqOUn9Ds9s4%3D"
+        
+        let account = try! AZSCloudStorageAccount(fromConnectionString: "SharedAccessSignature=" + sas + ";BlobEndpoint=https://boephotostore.blob.core.windows.net")
         
         blobClient = account.getBlobClient()
         
@@ -29,82 +31,79 @@ class HistoryViewController: UIViewController, UICollectionViewDelegate, UIColle
             fromName:"photocontainer"
         );
         
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let client = delegate.client!;
+        
+        let imageTable = client.table(withName: "Image");
+        
         var imagePaths = [String]();
         
-        imagePaths.append("user/2017-04-18-17-54-44--10304991.jpeg")
-        imagePaths.append("user/2017-04-18-17-54-48--536690126.jpeg")
-        imagePaths.append("user/2017-04-19-00-11-54--467650453.jpeg")
-        imagePaths.append("user/2017-04-19-00-11-59--10304991.jpeg")
-        imagePaths.append("user/2017-04-19-00-12-13--536690126.jpeg")
-        imagePaths.append("user/2017-04-19-00-12-17--1516264518.jpeg")
-        imagePaths.append("user/2017-04-19-00-46-48--467650453.jpeg")
-        imagePaths.append("user/2017-04-19-07-24-41--467650453.png")
-        imagePaths.append("user/2017-04-19-11-00-33--467650453.jpeg")
-        imagePaths.append("user/2017-04-19-11-02-57--10304991.jpeg")
-        imagePaths.append("user/2017-04-19-11-32-55--10304991.png")
-        imagePaths.append("user/2017-04-19-22-01-17--467650453.jpeg")
-        imagePaths.append("user/2017-04-19-22-01-25--10304991.jpeg")
-        imagePaths.append("user/cc6c3dd187fbeb301794ee8880b1183b.jpg")
-        imagePaths.append("user/images-1.jpeg")
-        imagePaths.append("user/images-2.jpeg")
-        imagePaths.append("user/images-3.jpeg")
-        imagePaths.append("user/images-4.jpeg")
-        imagePaths.append("user/images-5.jpeg")
-        imagePaths.append("user/images-6.jpeg")
-        imagePaths.append("user/images-7.jpeg")
-        imagePaths.append("user/images-8.jpeg")
-        imagePaths.append("user/images.jpeg")
-        imagePaths.append("user/images.png")
-        imagePaths.append("user/IMAGES_2017-04-19-00-41-00_-467650453.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-00-48-17_-467650453.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-10-13-24_-10304991.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-10-37-27_-467650453.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-10-39-30_-10304991.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-10-50-30_-467650453.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-10-57-58_-467650453.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-11-26-22_-467650453.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-11-28-45_-536690126.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-11-36-44_-467650453.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-11-36-57_-536690126.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-21-45-21_-467650453.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-21-54-11_-467650453.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-21-54-29_-10304991.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-21-54-41_-536690126.jpg")
-        imagePaths.append("user/IMAGES_2017-04-19-21-58-36_-467650453.jpg")
-        imagePaths.append("user/IMG_20160622_121558.jpg")
-        imagePaths.append("user/IMG_20161115_150729.jpg")
-        imagePaths.append("user/IMG_20170226_232817.jpg")
-        imagePaths.append("user/IMG_20170313_002415.jpg")
-        imagePaths.append("user/IMG_20170406_012106.jpg")
-        imagePaths.append("user/IMG_20170406_012219.jpg")
-        imagePaths.append("user/IMG_20170412_140517.jpg")
-        imagePaths.append("user/IMG_20170417_145700.jpg")
-        imagePaths.append("user/lol.PNG")
-        imagePaths.append("user/Nithf7m.jpg")
-        imagePaths.append("user/pairprogramming_f0d3ae7ef121e981e150bfcae4ecb995.jpg")
-        imagePaths.append("user/received_10212509993448566.jpeg")
-        imagePaths.append("user/Screenshot_2017-01-27-09-12-47.png")
-        imagePaths.append("user/Screenshot_2017-03-22-01-06-22.png")
-        imagePaths.append("user/Screenshot_2017-04-12-13-40-35.png")
-        imagePaths.append("user/Screenshot_2017-04-17-13-47-41.png")
-        imagePaths.append("user/user7145_pic154_1463771469.jpg")
+        //REPLACE WITH GLOBAL!
+        let userID = "user";
         
-        loadImages(paths: imagePaths);
+        let imgQuery = imageTable.query();
+        
+        imgQuery.order(byDescending: "createdAt");
+        
+        imgQuery.predicate = NSPredicate(format: "userID == %@", userID);
+        
+        imgQuery.read(completion: {
+            (result, error) in
+            if let err = error {
+                print("Error ", err);
+            } else if let imgResults = result?.items {
+                for img in imgResults {
+                    var path = img["id"] as! String
+                    
+                    path = path.replacingOccurrences(
+                        of: "_",
+                        with: "/",
+                        options: NSString.CompareOptions.literal,
+                        range: path.range(of: "_")
+                    )
+                    
+                    path = self.blobContainer.storageUri.primaryUri.absoluteString
+                        + "/"
+                        + path
+                        + "?"
+                        + sas;
+                    
+                    imagePaths.append(path);
+                }
+                
+                self.loadImages(paths: imagePaths);
+            }
+        })
     }
     
     func loadImages(paths: [String]){
+//        var index = 0;
+        
         for path in paths{
-            print(path);
-            
-            let image = blobContainer.blockBlobReference(fromName: path);
-            
-            image.downloadToData { (error, data) in
-                self.imageArray.append(UIImage(data: data!)!)
                 
-                self.historyCollectionView.reloadData();
-                
-                print("Downloaded Image");
+            DispatchQueue.main.async {
+                self.imageArray.append(UIImage(data: NSData(contentsOf: URL(string: path)!) as! Data)!)
+                self.historyCollectionView.reloadData()
             }
+            
+//            let image = blobContainer.blockBlobReference(fromName: path);
+//            
+//            image.downloadToData {
+//                (error, data) in
+//                    
+//                let uiImage = UIImage(data: data!)!
+//                
+//                print("Path: ", path, " At Index: ", index);
+//                
+//                self.imageArray.insert(uiImage, at: index)
+//                    
+//                DispatchQueue.main.async {
+//                    self.historyCollectionView.reloadData();
+//                }
+//                
+//                index += 1;
+//            }
         }
     }
 
