836000730eddd0778e680627fd9b3bc578205ae7
pgulegin <pgulegin@gmail.com>
2017-04-28 01:06:39 -0700

    Login Controller
    Login officially finished. Created an alert that would inform the user
    if they were not the previous user who was logged in.

--- PhotoArchive/LoginViewController.swift
+++ PhotoArchive/LoginViewController.swift
@@ -5,6 +5,8 @@
 //  Created by Phillip Gulegin on 1/26/17.
 //  Copyright © 2017 Phillip Gulegin. All rights reserved.
 //
+//  NOTE: Look for the 'DEBUG MODE' notes.
+//
 
 import UIKit
 
@@ -18,32 +20,96 @@ class LoginViewController: UIViewController {
     // Action 'Login' button on screen
     @IBAction func loginButton(_ sender: Any) {
         
-        // Executes if login was correct
+        // Continues executing if login was correct
         // Currently, to log in, leave input fields blank
-        if inputUsername.text == "" && inputPassword.text == "" {
+        if !(inputUsername.text == "" && inputPassword.text == "") {
             
-            // Sets up the application for the user
-            setup(user: inputUsername.text!)
-            
-            // Opens up the main storyboard
-            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-            
-            // Sets up a connection to the main tab controller
-            let resultViewController = storyBoard.instantiateViewController(withIdentifier: "TabBarController") as! TabBarController
-            
-            // Segues to the Dashboard View
-            self.present(resultViewController, animated:true, completion:nil)
-        }
-        // Executes if login was not correct
-        else {
-            // Creates an alert to notify the user the login was incorrect
+            // Creates an alert to notify user login was incorrect
             let alert = UIAlertController(title: "Alert", message: "Please enter correct Information", preferredStyle: UIAlertControllerStyle.alert)
             
-            // Creates the button prompt for the alert
-            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            // Creates button prompt for the alert
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { action in
+                
+                // Dismiss alert once user acknowledges
+                alert.dismiss(animated: true, completion: nil)
+            }))
             
-            // Present the alert to the user
+            // Present alert to the user
             self.present(alert, animated: true, completion: nil)
+            
+            // Return to the Login view
+            return
+        }
+        
+        // Establish defaults object to load persistent data
+        let defaults = UserDefaults.standard
+        
+        // Gets the pervious username, if there is one
+        let previousUser = defaults.object(forKey: UD.username) as? String
+        
+        // Executes if this is the first time a user has ever used the application
+        if previousUser == nil {
+            
+            // Sets up the application for first time use
+            setupFirstTime(user: inputUsername.text!)
+            
+            // Presents the main view controller to the rest of the application
+            presentMain()
+        }
+        // Executes if this application has been used before
+        else {
+            // Executes if current user is not previous user
+//            if inputUsername.text! != previousUser {
+            
+            
+                
+            // ----- START DEBUG MODE -----
+            // DELETE DEBUG MODE IN PRODUCTION
+            // Will not execute this entire if statement
+            // Delete this block for normal functionality
+            // Uncomment line 62 for normal functionality
+            if false {
+            // DELETE DEBUG MODE IN PRODUCTION
+            // ----- END DEBUG MODE -----
+                
+                
+                
+                // Alert to inform the user that because current user is not the previous user, all information on the application will be erased. Provide the option of cancelling the login process.
+                // Create alert
+                let alert = UIAlertController(title: "Different User", message: "This app is detecting that the user currently logging in is not the previous user which logged in last. Proceeding will erase all pending uploads and internal images.", preferredStyle: UIAlertControllerStyle.alert)
+                
+                // Add 'Proceed' button
+                alert.addAction(UIAlertAction(title: "Proceed", style: UIAlertActionStyle.destructive, handler: { action in
+                    
+                    // Dismiss the alert once user aknowledges
+                    alert.dismiss(animated: true, completion: nil)
+                    
+                    // Erase previous user's data
+                    self.setupNewUser(user: self.inputUsername.text!)
+                    
+                    // Presents the main view controller to the rest of the application
+                    self.presentMain()
+                }))
+                
+                // Add 'Cancel' button
+                alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: { action in
+                    
+                    // Dismiss the alert once user aknowledges
+                    alert.dismiss(animated: true, completion: nil)
+                    
+                    // Return to the Login view
+                    return
+                }))
+                
+                // Present the alert
+                self.present(alert, animated: true, completion: nil)
+            }
+            // Executes if current user is previous user
+            else {
+                
+                // Load as normal because current user is previous user
+                presentMain()
+            }
         }
     }
     
@@ -71,97 +137,103 @@ class LoginViewController: UIViewController {
     // MARK: - User Functions
     
     /**
-     Sets up the environment for the user. 
-     Takes into consideration if this is the first time the application is being used, if this is the same user logging in from before or if this is a new user logging in.
-     
-     - Parameter username:   The username of the user.
-     */
-    func setup(user: String) {
+     Presents the next view to the main application.
+    */
+    func presentMain() {
+        // Opens up the main storyboard
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
         
+        // Sets up a connection to the main tab controller
+        let resultViewController = storyBoard.instantiateViewController(withIdentifier: "TabBarController") as! TabBarController
+        
+        // Segues to the Dashboard View
+        self.present(resultViewController, animated:true, completion:nil)
+    }
+    
+    /**
+     Sets up the environment for the user. Assumes the application has never been used.
+     
+     - Parameter user: The username of the user.
+     */
+    func setupFirstTime(user: String) {
         // Establish defaults object to load persistent data
         let defaults = UserDefaults.standard
         
-        // Gets the pervious username, if there is one
-        let perviousUser = defaults.object(forKey: UD.username) as? String
+        // Save the user to defaults
+        defaults.set(user, forKey: UD.username)
         
-        // Executes if this is the first time a user has ever used the application
-        if perviousUser == nil {
-            
-            // Save the user to defaults
-            defaults.set(user, forKey: UD.username)
-            
-            // Create new directory in the 'Documents' directory
-            // Establish FileManager object
-            let fileMngr = FileManager.default
-            
-            // Establish path to 'Documents'
-            let docPath = fileMngr.urls(for: .documentDirectory, in: .userDomainMask)[0]
-            
-            // Establish path for 'CameraImages' in 'Documents'
-            let cameraImagesPath = docPath.appendingPathComponent("CameraImages").path
-            
-            // Create the 'CameraImages' Folder
-            do {
-                try fileMngr.createDirectory(atPath: cameraImagesPath, withIntermediateDirectories: true, attributes: nil)
-            } catch {
-                print("Error creating 'CameraImages' directory.")
-            }
-            
-            // Establish path for 'UploadImages' in 'Documents'
-            let uploadImagesPath = docPath.appendingPathComponent("UploadImages").path
-            
-            // Create the 'UploadImages' Folder
-            do {
-                try fileMngr.createDirectory(atPath: uploadImagesPath, withIntermediateDirectories: true, attributes: nil)
-            } catch {
-                print("Error creating 'UploadImages' directory.")
-            }
+        // Create new directory in the 'Documents' directory
+        // Establish FileManager object
+        let fileMngr = FileManager.default
+        
+        // Establish path to 'Documents'
+        let docPath = fileMngr.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        
+        // Establish path for 'CameraImages' in 'Documents'
+        let cameraImagesPath = docPath.appendingPathComponent("CameraImages").path
+        
+        // Create the 'CameraImages' Folder
+        do {
+            try fileMngr.createDirectory(atPath: cameraImagesPath, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+            print("Error creating 'CameraImages' directory.")
         }
-        // Executes if this application has been used before
-        else {
-            // Executes if current user is not previous user
-            if user != perviousUser {
-            
-                // Save the new user to defaults
-                defaults.set(user, forKey: UD.username)
+        
+        // Establish path for 'UploadImages' in 'Documents'
+        let uploadImagesPath = docPath.appendingPathComponent("UploadImages").path
+        
+        // Create the 'UploadImages' Folder
+        do {
+            try fileMngr.createDirectory(atPath: uploadImagesPath, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+            print("Error creating 'UploadImages' directory.")
+        }
+    }
+    
+    /**
+     Sets up the environment for a new user. Assumes that the application has been already used on this device.
+     
+     - Parameter user: The username of the user.
+     */
+    func setupNewUser(user: String) {
+        
+        // Establish defaults object to load persistent data
+        let defaults = UserDefaults.standard
                 
-                // Deleting everything in the 'CameraImages' and 'UploadImages' directories
-                // Establish FileManager object
-                let fileMngr = FileManager.default
-                
-                // Establish path to 'Documents'
-                let docPath = fileMngr.urls(for: .documentDirectory, in: .userDomainMask)[0]
-                
-                // Establish path for 'CameraImages' in 'Documents'
-                let cameraImagesPath = docPath.appendingPathComponent("CameraImages").path
-                
-                // Delete everything in 'CameraImages' Folder
-                do {
-                    let filePaths = try fileMngr.contentsOfDirectory(atPath: cameraImagesPath)
-                    for filePath in filePaths {
-                        try fileMngr.removeItem(atPath: cameraImagesPath + "/" + filePath)
-                    }
-                } catch {
-                    print("Error deleting files in 'CameraImages' directory")
-                }
-                
-                // Establish path for 'UploadImages' in 'Documents'
-                let uploadImagesPath = docPath.appendingPathComponent("UploadImages").path
-                
-                // Delete everything in 'UploadImages' Folder
-                do {
-                    let filePaths = try fileMngr.contentsOfDirectory(atPath: uploadImagesPath)
-                    for filePath in filePaths {
-                        try fileMngr.removeItem(atPath: uploadImagesPath + "/" + filePath)
-                    }
-                } catch {
-                    print("Error deleting files in 'UploadImages' directory")
-                }
+        // Save the new user to defaults
+        defaults.set(user, forKey: UD.username)
+        
+        // Deleting everything in the 'CameraImages' and 'UploadImages' directories
+        // Establish FileManager object
+        let fileMngr = FileManager.default
+        
+        // Establish path to 'Documents'
+        let docPath = fileMngr.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        
+        // Establish path for 'CameraImages' in 'Documents'
+        let cameraImagesPath = docPath.appendingPathComponent("CameraImages").path
+        
+        // Delete everything in 'CameraImages' Folder
+        do {
+            let filePaths = try fileMngr.contentsOfDirectory(atPath: cameraImagesPath)
+            for filePath in filePaths {
+                try fileMngr.removeItem(atPath: cameraImagesPath + "/" + filePath)
             }
-            // Executes if current user is previous user
-            else {
-                // Do nothing... 
+        } catch {
+            print("Error deleting files in 'CameraImages' directory")
+        }
+        
+        // Establish path for 'UploadImages' in 'Documents'
+        let uploadImagesPath = docPath.appendingPathComponent("UploadImages").path
+        
+        // Delete everything in 'UploadImages' Folder
+        do {
+            let filePaths = try fileMngr.contentsOfDirectory(atPath: uploadImagesPath)
+            for filePath in filePaths {
+                try fileMngr.removeItem(atPath: uploadImagesPath + "/" + filePath)
             }
+        } catch {
+            print("Error deleting files in 'UploadImages' directory")
         }
     }
 }
